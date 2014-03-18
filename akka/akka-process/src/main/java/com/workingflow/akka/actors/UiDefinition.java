0d8b8b72e03a4de780c15144a7d1b267dbef2b3f
Cristian Rinaldi <csrinaldi@gmail.com>
2014-03-18 15:12:06 -0300

    Ejemplo de uso de annotationprocesor

---
+++ akka/akka-process/src/main/java/com/workingflow/akka/actors/UiDefinition.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.workingflow.akka.actors;
+
+import com.workinflow.tool.annprocesor.Resource;
+
+/**
+ *
+ * @author csrinaldi
+ */
+@Resource(resourcePath = "src/main/resources/assets")
+public interface UiDefinition {
+    
+}
